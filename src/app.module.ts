6952c223bd0bbbbce53dfef614e8feff22b4200f
Loic-4-HC <loic.lobe@hellocomputing.tech>
2022-12-08 17:41:22 +0100

    create user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './typeorm.config';
 import { PermissionModule } from './permission/permission.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PermissionModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PermissionModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
