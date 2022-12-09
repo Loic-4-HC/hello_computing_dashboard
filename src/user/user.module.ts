a730cbf5096f05dbbe63718c3e24c23fac92e10e
Loic-4-HC <loic.lobe@hellocomputing.tech>
2022-12-09 09:38:01 +0100

    setting up user.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './controller/user.controller';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [
+    {
+      provide: 'USER_SERVICE',
+      useClass: UserService,
+    },
+    // {
+    //   provide: APP_FILTER,
+    //   useClass: HttpExceptionFilter,
+    // },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class UserModule {}
