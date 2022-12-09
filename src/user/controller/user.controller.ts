376225bd87330e3ba03d89791c35595cc4259aa6
Loic-4-HC <loic.lobe@hellocomputing.tech>
2022-12-09 09:37:04 +0100

    setting up user controller

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,34 +1,125 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiTags, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UserEntity } from '../entities/user.entity';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
+@ApiTags('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    @Inject('USER_SERVICE')
+    private readonly userService: UserService,
+  ) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @ApiParam({
+    name: 'createUserDto',
+    required: true,
+    description: 'it must be a new user',
+    type: CreateUserDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'A new user was succesfully created',
+    type: UserEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Input.',
+  })
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  // @ApiResponse({
+  //   status: 201,
+  //   description: 'A new user was succesfully created',
+  //   type: CreateUserDto,
+  // })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Input.',
+  })
+  async findAll() {
+    return await this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'id of the researched user',
+  })
+  // @ApiResponse({
+  //   status: 201,
+  //   description: 'A new user was succesfully created',
+  //   type: UserEntity,
+  // })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Input.',
+  })
+  async findOne(@Param('id') id: string) {
+    return await this.userService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'id of the ',
+    type: UpdateUserDto,
+  })
+  @ApiParam({
+    name: 'updateUserDto',
+    required: true,
+    description: 'updated user information',
+    type: UpdateUserDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'A new user was succesfully created',
+    type: UserEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Input.',
+  })
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.userService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @ApiParam({
+    name: 'CreateUserDto',
+    required: true,
+    description: 'it must be a new user',
+    type: CreateUserDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'A new UserEntity was succesfully created',
+    type: UserEntity,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Input.',
+  })
+  async remove(@Param('id') id: string) {
+    return await this.userService.remove(+id);
   }
 }
