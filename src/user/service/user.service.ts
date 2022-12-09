3ee701f77ad966b3ddcecab0fca9a882905b8e37
Loic-4-HC <loic.lobe@hellocomputing.tech>
2022-12-09 09:39:12 +0100

    setting up  user service

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CustomException } from '../../utils/custom-exeption.filter';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UserEntity } from '../entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async findUserByEmail(email: string): Promise<UserEntity> {
+    return await this.userRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async create(createUserDto: CreateUserDto) {
+    const isUserExisting = await this.findUserByEmail(createUserDto.email);
+
+    if (isUserExisting !== null) {
+      // user already exisiting
+      throw new CustomException(
+        'invalid input : user already existing',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // new user
+    const newUser = this.userRepository.create(createUserDto);
+    return await this.userRepository.save(newUser);
   }
 
-  findOne(id: number) {
+  async findAll() {
+    const users = await this.userRepository.find();
+
+    if (users === null) {
+      throw new CustomException('No user found', HttpStatus.NOT_FOUND);
+    }
+
+    return users;
+  }
+
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const isUserExisting = await this.findUserByEmail(updateUserDto.email);
+
+    if (isUserExisting !== null) {
+      // user already exisiting
+      throw new CustomException(
+        'invalid input : user already existing',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return this.userRepository.update(id, updateUserDto);
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
