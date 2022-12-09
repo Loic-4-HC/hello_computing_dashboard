ca8dc7ae44e396dd3394f026858ac7a7ab6ab485
Loic-4-HC <loic.lobe@hellocomputing.tech>
2022-12-09 10:24:35 +0100

    update validator error message in CreatePermissionDto

--- src/permission/dto/create-permission.dto.ts
+++ src/permission/dto/create-permission.dto.ts
@@ -7,20 +7,20 @@ export class CreatePermissionDto {
     example: 'product:create',
   })
   @IsString({
-    message: 'must be a string',
+    message: 'name must be a string',
   })
   @MinLength(3, {
-    message: 'name should be at least 3 characters',
+    message: 'name must be at least 3 characters',
   })
   // @Matches(/([a-z]:(create|update|delete))$/, {
   @Matches(/(\w:\w)/, {
-    message: 'name does respect the norm. ex : product:create',
+    message: 'name must respect the norm. ex : product:create',
   })
   name: string;
 
   @IsOptional()
   @MinLength(8, {
-    message: 'description should be at least 8 characters',
+    message: 'description must be at least 8 characters',
   })
   description?: string;
 }
