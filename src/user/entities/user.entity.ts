84baacfd91d2023a312b9672d59aea491a34f582
Loic-4-HC <loic.lobe@hellocomputing.tech>
2022-12-09 09:35:47 +0100

    setting up user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,75 @@
-export class User {}
+import { CreateAdresseDto } from 'src/adresse/dto/create-adresse.dto';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  uid: number;
+
+  @Column({
+    type: 'varchar',
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    name: 'first_name',
+    type: 'varchar',
+  })
+  firstName = '';
+
+  @Column({
+    name: 'last_name',
+    type: 'varchar',
+    default: '',
+  })
+  lastName = '';
+
+  @Column({
+    name: 'full_name',
+    type: 'varchar',
+    default: '',
+  })
+  fullName = `${this.firstName} ${this.lastName}`;
+
+  @Column({
+    name: 'profil_img',
+  })
+  profilImage: string;
+
+  @Column({
+    name: 'phone_number',
+    type: 'varchar',
+    nullable: true,
+  })
+  phoneNumber: string;
+
+  @Column({
+    name: 'adresse',
+    type: 'varchar',
+    nullable: true,
+  })
+  adresse: CreateAdresseDto;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    // default: () => 'CURRENT_TIMESTAkMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+    nullable: true,
+  })
+  updatedAt: Date;
+}
